ac4a9f4f41a003716540c8da128b6c285e0e6c6b
kurose-kira <yotuki.008@gmail.com>
2026-02-26 23:09:21 +0900

    Projects css setting and Projects auto fix

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,32 @@
+import "./Projects.css";
+
 function Projects() {
+  const projects = [
+    {
+      title: "Project 1",
+      description: "Description 1",
+      url: "https://github.com/kurose-kira/project-1",
+    },
+    {
+      title: "Project 2",
+      description: "Description 2",
+      url: "https://github.com/kurose-kira/project-2",
+    },
+  ];
+
   return (
     <section className="glass-panel">
       <h2>Projects</h2>
 
-      <div className="social-links">
-        <article>aaa</article>
+      <div className="projects-list">
+        {projects.map((project) => (
+          <a href={project.url}>
+            <article key={project.title}>
+              <h3>{project.title}</h3>
+              <p>{project.description}</p>
+            </article>
+          </a>
+        ))}
       </div>
     </section>
   );
